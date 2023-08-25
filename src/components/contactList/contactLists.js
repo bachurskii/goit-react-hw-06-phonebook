5e909429b0a7030d5c7243d5556921c467f08f51
Georgen1 <123319264+bachurskii@users.noreply.github.com>
2023-08-25 14:31:48 +0300

    Update contactLists.js

--- src/components/contactList/contactLists.js
+++ src/components/contactList/contactLists.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { removeContact } from 'redux/slice/slice';
 import { useDispatch } from 'react-redux';
@@ -6,20 +6,35 @@ import { useDispatch } from 'react-redux';
 const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts.list);
+  const [filter, setFilter] = useState('');
 
   const handleDelete = id => {
     dispatch(removeContact(id));
   };
 
+  const filteredContacts = contacts.filter(
+    contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.phone.includes(filter)
+  );
+
   return (
-    <ul>
-      {contacts.map(contact => (
-        <li key={contact.id}>
-          {contact.name} - {contact.phone}
-          <button onClick={() => handleDelete(contact.id)}>Delete</button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <input
+        type="text"
+        value={filter}
+        onChange={e => setFilter(e.target.value)}
+        placeholder="Filter contacts"
+      />
+      <ul>
+        {filteredContacts.map(contact => (
+          <li key={contact.id}>
+            {contact.name} - {contact.phone}
+            <button onClick={() => handleDelete(contact.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
